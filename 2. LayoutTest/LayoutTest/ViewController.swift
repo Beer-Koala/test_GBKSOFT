74e649cc7dce6e8b554f0f094ea47640b4f1f857
first commit <>
2019-06-16 02:51:14 +0300

    Some moving in storyboard in second project

---
+++ 2. LayoutTest/LayoutTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutTest
+//
+//  Created by BeerKoala on 6/16/19.
+//  Copyright © 2019 a.kryvchykov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
